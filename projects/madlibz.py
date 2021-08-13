d5813b95e5e2d900f87141abee97265ad607eb3a
Justin DeRaymond <jusderay@amazon.com>
2021-08-13 14:10:35 +0000

    Added a welcome screen and looped for continuos playing

--- projects/madlibz.py
+++ projects/madlibz.py
@@ -3,6 +3,17 @@
 import requests
 
 def init():
+    print("""
+            _____________________________________
+            |                                   |
+            |                                   |
+            |                                   |
+            |       Welcome to MADLIBZ!!        |
+            |                                   |
+            |                                   |
+            |___________________________________|
+
+""")
     #minlength set to 2 so that the madlib "Hello ______!" will not be populated
     minlength = 2
     maxlength = input("What is the maximum number of blanks you would like to fill in?: ")
@@ -28,36 +39,39 @@ def init():
     return title, blanks, values
 
 def main():
-    title, blanks, values = init()
-    user_answers = []
-    for blank in blanks:
-        if blank[0] == 'a' or blank[0] == 'e' or blank[0] == 'i' or blank[0] == 'o' or blank[0] == 'u':
-            answer = input(f"Provide an {blank}: ")
-            while answer in user_answers:
-                answer = input(f"You already used {answer} as a response. Try a new {blank} for maximum fun! ")
-            user_answers.append(answer)
-        else:
-            answer = input(f"Provide a {blank}: ")
-            while answer in user_answers:
-                answer = input(f"You already used {answer} as a response. Try a new {blank} for maximum fun! ")
-            user_answers.append(answer)
-    count = 0
-    index = 1
-    while count < len(values):
-        for line in user_answers:
-            values.insert(index, line)
-            index += 2
-            count += 3
+    play_again = 'yes'
+    while play_again == 'yes':
+        title, blanks, values = init()
+        user_answers = []
+        for blank in blanks:
+            if blank[0] == 'a' or blank[0] == 'e' or blank[0] == 'i' or blank[0] == 'o' or blank[0] == 'u':
+                answer = input(f"Provide an {blank}: ")
+                while answer in user_answers:
+                    answer = input(f"You already used {answer} as a response. Try a new {blank} for maximum fun! ")
+                user_answers.append(answer)
+            else:
+                answer = input(f"Provide a {blank}: ")
+                while answer in user_answers:
+                    answer = input(f"You already used {answer} as a response. Try a new {blank} for maximum fun! ")
+                user_answers.append(answer)
+        count = 0
+        index = 1
+        while count < len(values):
+            for line in user_answers:
+                values.insert(index, line)
+                index += 2
+                count += 3
 
-    textfile = open("madlibz.txt", "w+")
-    textfile.write('\n' + title + '\n')
-    for word in values:
-        textfile.write(word)
-    textfile.close
+        textfile = open("madlibz.txt", "w+")
+        textfile.write('\n' + title + '\n')
+        for word in values:
+            textfile.write(word)
+        textfile.close
 
-    textfile = open("madlibz.txt", "rt")
-    result = textfile.read()
-    print(result)
+        textfile = open("madlibz.txt", "rt")
+        result = textfile.read()
+        print(result)
+        play_again = input("Do you want to play again? (yes or no) ")
 
 if __name__ == "__main__":
     main()
